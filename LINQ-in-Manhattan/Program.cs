d9ff8d2817d05898a03fdd9e51961bdec619f28c
YamanAyoun <yamanayoun0@gmail.com>
2023-07-11 01:21:41 +0300

    Add project files.

---
+++ LINQ-in-Manhattan/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LINQ_in_Manhattan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filter();
+        }
+        public class RootObject
+        {
+            public string type { get; set; }
+            public Feature[] features { get; set; }
+        }
+
+        public class Feature
+        {
+            public string type { get; set; }
+            public Geometry geometry { get; set; }
+            public Properties properties { get; set; }
+        }
+
+        public class Geometry
+        {
+            public string type { get; set; }
+            public float[] coordinates { get; set; }
+        }
+
+        public class Properties
+        {
+            public string zip { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string address { get; set; }
+            public string borough { get; set; }
+            public string neighborhood { get; set; }
+            public string county { get; set; }
+        }
+
+        public static void Filter()
+        {
+            
+            using (StreamReader reader = File.OpenText("../../../data.json"))
+
+            {   
+                JObject dataFile = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+                
+                var result = JsonConvert.DeserializeObject<RootObject>(dataFile.ToString());
+
+                
+                var neighborhoods = from n in result.features
+                                    select n.properties.neighborhood;
+                Console.WriteLine($"All neighborhoods by using LINQ query statements: {neighborhoods.Count()}.");
+                
+                var filteredNeighborhoods = from n in neighborhoods
+                                            where (n != "")
+                                            select n;
+                Console.WriteLine($"Filtered neighborhoods by using LINQ query statements: {filteredNeighborhoods.Count()}.");
+               
+                
+            }
+        }
+
+        
+    }
+    }
